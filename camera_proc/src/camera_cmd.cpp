a190ce7d2dbf63334ee1afecd1eeaee4f7a81364
Xiaozheng Xu <xiaozheng.xu@students.olin.edu>
2016-12-06 21:28:02 -0500

    added camera obstacle avoiding code

--- camera_proc/src/camera_cmd.cpp
+++ camera_proc/src/camera_cmd.cpp
@@ -10,14 +10,44 @@
 
 ros::Publisher pub;
 
+//camera screen is 640 * 480
+void messageCallback(const std_msgs::Int16MultiArray input){
+    std::vector<int> left;
+    std::vector<int> right;
+    std::vector<int> center;
+    geometry_msgs::Twist msg;
+    msg.linear.x = 0;
 
-void messageCallback(const std_msgs::Int16MultiArray msg){
+    for (int i = 0; i<input.data.size(); i+=4){
+        float x = input.data[i];
+        float width = input.data[i+2];
+        float centerx = x + width/2;
 
-    geometry_msgs::Twist msg_twist;
+        if (centerx < 260){
+            left.push_back(x+width);
+        }else if (centerx>=260 && centerx <= 380){
+            center.push_back(x, x+width); //center always have left most and right most
+        }else if (centerx> 380){
+            right.push_back(x);
+        }
 
-    pub.publish(msg_twist);
+    }
+    if (left.size() == 0 && center.size() == 0 && right.size() == 0){// if there's no obstacles
+        msg.angular.z = 0;
+    }else if (center.size() == 0){ // there might be obstacle on the right or left, but not in the middle, so we can go straight
+        msg.angular.z = 0;
+        std::cout << "I'm going straight, but there might be obstacles beside me" << std::endl;
+    }else if (left.size() > 0 && center.size() > 0 && right.size() == 0){
+        msg.angular.z = 1.0;
+        std::cout << "I'm turning right" << std::endl;
+    }else if (right.size()>0 && center.size()> 0 && left.size() == 0){
+        msg.angular.z = -1.0;
+        std::cout << "I'm turning left" <<std::endl;
+    }else{ //if there's obstacles everywhere, just try going straight, and scream on the inside
+        msg.angular.z = 0;
+    }
 
-
+    pub.publish(msg);
 }
 
 
